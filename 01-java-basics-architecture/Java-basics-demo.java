5fde3f295045e7dea383025d79fb7ef5255fda2e
vidya <137021847+Mynameisvidya@users.noreply.github.com>
2026-02-02 16:26:56 +0530

    Add basic Java program demonstrating platform independence

---
+++ 01-java-basics-architecture/Java-basics-demo.java
@@ -0,0 +1,11 @@
+public class JavaBasics {
+
+    public static void main(String[] args) {
+
+        // Basic Java Program
+        System.out.println("Java is Platform Independent");
+
+        // Program execution always starts from main method
+        System.out.println("Program starts from main()");
+    }
+}
